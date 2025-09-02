28046350901f604f0d44502cd5c35ecfb59f6e8f
RENE OLIVEIRA <37373571+raoever@users.noreply.github.com>
2025-09-02 09:41:31 -0300

    Delete Checklist Automático directory

--- Checklist Automático/src/temp_fix.jsx
+++
@@ -1,9 +0,0 @@
-// Temporary script to fix the SELECT issue by using "NONE" instead of empty string
-const content = `
-// All SelectItem with value="" should be changed to value="NONE"
-// All onValueChange with '' should include 'NONE'
-// All status checks for '' should be changed to 'NONE' 
-`;
-
-// The issue is that Radix UI doesn't allow empty string values in SelectItem
-// So we need to use "NONE" instead of empty string for the "not verified" state
